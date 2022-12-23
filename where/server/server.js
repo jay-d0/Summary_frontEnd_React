5af5689c48c8d70f646b98a067e9ce22ee273ea3
uuu_suk_ <71217304+Song-woo-seok@users.noreply.github.com>
2022-12-23 15:01:19 +0900

    Update server.js

--- where/server/server.js
+++ where/server/server.js
@@ -4,7 +4,8 @@ const port = 3001;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("./server/dong_coords.json", "utf8"));
+const data = JSON.parse(fs.readFileSync("./server/dong.json", "utf8"));
+const data2 = JSON.parse(fs.readFileSync("./server/summary.json", "utf8"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -12,7 +13,8 @@ app.use(bodyParser.json());
 
 app.post("/info", (req, res) => {
   console.log(data[req.body.id]);
-  res.send(data[req.body.id])
+  console.log(data2[req.body.id]);
+  res.send([data[req.body.id], data2[req.body.id]])
 })
 
 app.listen(port, () => {
